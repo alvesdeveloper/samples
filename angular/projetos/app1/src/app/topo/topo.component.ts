80fad3e42ce39728ae286006ea2539897c2f5170
alvesdeveloper <alves.alves86@gmail.com>
2018-06-28 01:29:38 -0300

    inicio angular

---
+++ angular/projetos/app1/src/app/topo/topo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+/*o decorator abaixo é que torna a classe abaixo um componente e encaminha pra classe alguns metadados*/
+/* conseguimos usar este decorator porque fizemos o import do modulo "component", acima*/
+@Component({
+    selector: 'app-topo', /*indica qual será o rotulo usado para instancia desse componente*/
+    templateUrl: './topo.component.html' /*indica qual o template que a classe abaixo controlará*/
+
+})
+
+export class TopoComponent {}
